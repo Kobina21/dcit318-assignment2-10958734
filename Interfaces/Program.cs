13a7e373da874e5dba3000d264aa5dcafd3b4f48
Kobina21 <rkaforson@st.ug.edu.gh>
2025-08-09 10:02:51 -0400

    commit

---
+++ Interfaces/Program.cs
@@ -0,0 +1,36 @@
+namespace Interfaces;
+
+public interface IMovable
+{
+    void Move();
+}
+
+public class Car : IMovable
+{
+    public void Move()
+    {
+        Console.WriteLine("Car is moving");
+    }
+}
+
+public class Bicycle : IMovable
+{
+    public void Move()
+    {
+        Console.WriteLine("Bicycle is moving");
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        IMovable car = new Car();
+        IMovable bicycle = new Bicycle();
+
+        car.Move();
+        bicycle.Move();
+    }
+}
+
+
